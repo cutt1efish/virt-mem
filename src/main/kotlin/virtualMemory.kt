a0fbcc914fc4efe9e24d572b182ea003573d6878
cutt1efish <alexandr2280303@gmail.com>
2020-09-30 14:10:52 +0300

    реализовал FIFO

--- src/main/kotlin/virtualMemory.kt
+++ src/main/kotlin/virtualMemory.kt
@@ -1,10 +1,33 @@
 import java.io.File
+import javax.print.attribute.standard.MediaSizeName
 import kotlin.system.exitProcess
 
 fun readFileAsLinesUsingUseLines(fileName: String): MutableList<String>
         = File(fileName).useLines { it.toMutableList() }
 
-
+fun fifo(commands: MutableList<Int>, memorySize: Int, pagesCount: Int): MutableList<Int>{
+    val changeList = mutableListOf<Int>()
+    val memory = mutableListOf<Int>()
+    val pages = Array(pagesCount) { 0 }
+    while (memory.size < memorySize ) {
+        memory.add(memory.size, commands[0])
+        pages[commands[0]] += 1
+        commands.removeAt(0)
+        changeList.add(memory.size-1)
+    }
+    var firstPage = 0
+    for (i in commands) {
+        if (memory.find { it == i } == null ) {
+            memory[firstPage] = i
+            changeList.add(firstPage)
+            pages[i-1] += 1
+            firstPage += 1
+            if (firstPage == memorySize) firstPage = 0
+        }
+        else changeList.add(-1)
+    }
+    return changeList
+}
 
 fun main() {
     val conf = readFileAsLinesUsingUseLines("inpout/conf.txt")[0].split(' ')
@@ -16,9 +39,12 @@ fun main() {
     }
 
     val commandsString = readFileAsLinesUsingUseLines("inpout/commands.txt")
-    var commands = mutableListOf<Int>()
+    val commands = mutableListOf<Int>()
     for (i in commandsString) {
         commands.add(i.toInt())
     }
 
+    for (i in fifo(commands, memorySize, pagesCount)) {
+        println(i)
+    }
 }
