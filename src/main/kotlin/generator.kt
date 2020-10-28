05ede4e526bd2f34498648addc1ae3f998b8a475
cutt1efish <alexandr2280303@gmail.com>
2020-10-28 17:55:25 +0300

    сделал рефакторинг virtualMemory.kt и немного исправил генератор

--- src/main/kotlin/generator.kt
+++ src/main/kotlin/generator.kt
@@ -7,12 +7,11 @@ fun main() {
     val commands = File("generator/commands.txt")
     if (commands.exists()) commands.delete()
 
-    val gen = File("generator/gen.txt")
     val confMax: Int
     val pagesMax: Int
     val commandsMax: Int
     if (conf.exists()) {
-        val maxs = readFileAsLinesUsingUseLines("generator/gen.txt")
+        val maxs = fileToList("generator/gen.txt")
         confMax = maxs[0].toInt()
         pagesMax = maxs[1].toInt()
         commandsMax = maxs[2].toInt()
@@ -23,10 +22,10 @@ fun main() {
         commandsMax = 10000
     }
 
-    val confContent = "${Random.nextInt(0 , 100)}"
+    val confContent = "${Random.nextInt(0 , confMax)}"
     var commandsContent = ""
-    val pagesCount = Random.nextInt(confContent.toInt(), 1000)
-    for (i in 0..Random.nextInt(pagesCount, 10000)) {
+    val pagesCount = Random.nextInt(confContent.toInt(), pagesMax)
+    for (i in 0..Random.nextInt(pagesCount, commandsMax)) {
         commandsContent += "${Random.nextInt(0, pagesCount) + 1}\n"
     }
 
--- src/main/kotlin/virtualMemory.kt
+++ src/main/kotlin/virtualMemory.kt
@@ -1,60 +1,49 @@
 import java.io.File
-import kotlin.system.exitProcess
 
-fun readFileAsLinesUsingUseLines(fileName: String): MutableList<String>
-        = File(fileName).useLines { it.toMutableList() }
+fun fileToList(fileName: String): MutableList<String> {
+    return File(fileName).useLines { it.toMutableList() }
+}
 
 fun fifo(comms: MutableList<Int>, memorySize: Int): MutableList<Int>{
-    val commands = comms.toMutableList()
-    val changeList = mutableListOf<Int>()
-    val memory = mutableListOf<Int>()
-    while (memory.size < memorySize ) {
-        if (memory.find { it == commands[0] } == null) {
-            memory.add(memory.size, commands[0])
-            commands.removeAt(0)
-            changeList.add(memory.size)
-        }
-        else {
-            changeList.add(-1)
-            commands.removeAt(0)
-        }
-    }
+    val commands = comms.toMutableList() // создаю копию, чтоб не менялся оригинал
+    val changesList = mutableListOf<Int>()
+    val memory = mutableListOf<Int>() // в каждой ячейке памяти будет номер страницы
+
+    makeMemoryFullFifo(memory, memorySize, commands, changesList)
+
     var firstPage = 0
     for (i in commands) {
         if (memory.find { it == i } == null ) {
             memory[firstPage] = i
-            changeList.add(firstPage + 1)
+            changesList.add(firstPage + 1)
             firstPage += 1
             if (firstPage == memorySize) firstPage = 0
         }
-        else changeList.add(-1)
+        else changesList.add(-1)
     }
-    return changeList
+    return changesList
 }
 
-fun lru (comms: MutableList<Int>, memorySize: Int): MutableList<Int> {
-    val commands = comms.toMutableList()
-    val changeList = mutableListOf<Int>()
-    val memory = mutableListOf<Pair<Int, Int>>()
+fun makeMemoryFullFifo(memory: MutableList<Int>, memorySize: Int, commands: MutableList<Int>, changesList: MutableList<Int>) {
     while (memory.size < memorySize) {
-        for (i in 0 until memory.size) {
-            memory[i] = Pair(memory[i].first, memory[i].second + 1)
-        }
-        if (memory.find { it.first == commands[0] } == null) {
-            memory.add(commands[0] to 0)
+        if (memory.find { it == commands[0] } == null) {
+            memory.add(memory.size, commands[0])
             commands.removeAt(0)
-            changeList.add(memory.size)
-        }
-        else {
-            changeList.add(-1)
-            for (i in 0 until memory.size) {
-                if (memory[i].first == commands[0]) {
-                    memory[i] = Pair(memory[i].first, 0)
-                    commands.removeAt(0)
-                }
-            }
+            changesList.add(memory.size)
+        } else {
+            changesList.add(-1)
+            commands.removeAt(0)
         }
     }
+}
+
+fun lru(comms: MutableList<Int>, memorySize: Int): MutableList<Int> {
+    val commands = comms.toMutableList()
+    val changesList = mutableListOf<Int>()
+    val memory = mutableListOf<Pair<Int, Int>>() // первый аргумент в паре отвечает за номер страницы, второй аргумент отвечает за время, которое к этой странице не обращались
+                                                 // одно изменение в памяти = одна единица времени
+    makeMemoryFullLru(memory, memorySize, commands, changesList)
+
     for (i in commands) {
         if (memory.find {it.first == i} == null) {
             var idxOfFirst = 0
@@ -69,10 +58,10 @@ fun lru (comms: MutableList<Int>, memorySize: Int): MutableList<Int> {
                 }
             }
             memory[idxOfFirst] = Pair(i, 0)
-            changeList.add(idxOfFirst + 1)
+            changesList.add(idxOfFirst + 1)
         }
         else {
-            changeList.add(-1)
+            changesList.add(-1)
             for (j in 0 until memory.size) {
                 memory[j] = Pair(memory[j].first, memory[j].second + 1)
                 if (memory[j].first == i) {
@@ -81,29 +70,49 @@ fun lru (comms: MutableList<Int>, memorySize: Int): MutableList<Int> {
             }
         }
     }
-    return changeList
+    return changesList
+}
+
+private fun makeMemoryFullLru(
+        memory: MutableList<Pair<Int, Int>>,
+        memorySize: Int,
+        commands: MutableList<Int>,
+        changesList: MutableList<Int>
+) {
+    while (memory.size < memorySize) {
+        for (i in 0 until memory.size) {
+            memory[i] = Pair(memory[i].first, memory[i].second + 1)
+        }
+        if (memory.find { it.first == commands[0] } == null) {
+            memory.add(commands[0] to 0)
+            commands.removeAt(0)
+            changesList.add(memory.size)
+        } else {
+            changesList.add(-1)
+            for (i in 0 until memory.size) {
+                if (memory[i].first == commands[0]) {
+                    memory[i] = Pair(memory[i].first, 0)
+                    commands.removeAt(0)
+                }
+            }
+        }
+    }
 }
 
 fun opt(comms: MutableList<Int>, memorySize: Int): MutableList<Int> {
     val commands = comms.toMutableList()
-    val changeList = mutableListOf<Int>()
-    val memory = mutableListOf<Pair<Int, Int>>()
-    while (memory.size < memorySize) {
-        if (memory.find { it.first == commands[0] } == null) {
-            memory.add(Pair(commands[0], -1))
-            changeList.add(memory.size)
-            commands.removeAt(0)
-        }
-        else {
-            commands.removeAt(0)
-            changeList.add(-1)
-        }
-    }
+    val changesList = mutableListOf<Int>()
+    val memory = mutableListOf<Pair<Int, Int>>() // здесь первый элемент в паре - номер страницы, второй элемент - следующее вхождение страницы
+                                                 // (если второй элемент -1, значит это вхождение последнее или следующее еще не найдено)
+
+    makeMemoryFullOpt(memory, memorySize, commands, changesList)
+
     for (i in 0 until commands.size) {
         if (memory.find { it.first == commands[i] } != null) {
-            changeList.add(-1)
+            changesList.add(-1)
         }
         else {
+            // ищем следующие запросы страниц
             for (j in i until commands.size) {
                 for (k in 0 until memorySize) {
                     if ((memory[k].first == commands[j]) and (memory[k].second == -1)) {
@@ -111,6 +120,7 @@ fun opt(comms: MutableList<Int>, memorySize: Int): MutableList<Int> {
                     }
                 }
             }
+            // ищем, что заменить
             var maximum = 0
             for (j in 0 until memorySize) {
                 if (memory[maximum].second == -1) break
@@ -119,52 +129,82 @@ fun opt(comms: MutableList<Int>, memorySize: Int): MutableList<Int> {
                 }
             }
             memory[maximum] = Pair(commands[i], -1)
-            changeList.add(maximum + 1)
+            changesList.add(maximum + 1)
         }
     }
-    return changeList
+    return changesList
 }
 
-fun main() {
-    val memorySize = readFileAsLinesUsingUseLines("inpout/conf.txt")[0].toInt()
-
-    val commandsString = readFileAsLinesUsingUseLines("inpout/commands.txt")
-    val commands = mutableListOf<Int>()
-    for (i in commandsString) {
-        commands.add(i.toInt())
+private fun makeMemoryFullOpt(
+        memory: MutableList<Pair<Int, Int>>,
+        memorySize: Int,
+        commands: MutableList<Int>,
+        changesList: MutableList<Int>
+) {
+    while (memory.size < memorySize) {
+        if (memory.find { it.first == commands[0] } == null) {
+            memory.add(Pair(commands[0], -1))
+            changesList.add(memory.size)
+            commands.removeAt(0)
+        } else {
+            commands.removeAt(0)
+            changesList.add(-1)
+        }
     }
+}
 
+fun makeAnswer(memorySize: Int, commands: MutableList<Int>) {
     val answer = File("inpout/answer.txt")
     if (answer.exists()) answer.delete()
     var answerContent = ""
 
-    var countOfAnswersFifo = 0
     answerContent += "Результат выполнения FIFO: \n"
-    for (i in fifo(commands, memorySize)) {
-        answerContent += i.toString() + "\n"
-        if (i != -1) countOfAnswersFifo++
-    }
-
-    var countOfAnswersLru = 0
+    var pair = partOfAnswerAndCountOf2Type(fifo(commands, memorySize))
+    val countOfAnswersFifo = pair.first
+    answerContent += pair.second
     answerContent += "___________________________ \n"
+
     answerContent += "Результат выполнения LRU: \n"
-    for (i in lru(commands, memorySize)) {
-        answerContent += i.toString() + "\n"
-        if (i != -1) countOfAnswersLru++
-    }
-
-    var countOfAnswersOpt = 0
+    pair = partOfAnswerAndCountOf2Type(lru(commands, memorySize))
+    val countOfAnswersLru = pair.first
+    answerContent += pair.second
     answerContent += "___________________________ \n"
+
     answerContent += "Результат выполнения OPT: \n"
-    for (i in opt(commands, memorySize)) {
-        answerContent += i.toString() + "\n"
-        if (i != -1) countOfAnswersOpt++
-    }
+    pair = partOfAnswerAndCountOf2Type(opt(commands, memorySize))
+    val countOfAnswersOpt = pair.first
+    answerContent += pair.second
     answerContent += "___________________________ \n"
-    answerContent += "Количество запросов второго типа: \n" +
-                     "FIFO $countOfAnswersFifo \n" +
-                     "LRU $countOfAnswersLru \n" +
-                     "OPT $countOfAnswersOpt \n"
-    answer.writeText(answerContent)
 
+    answerContent += "Количество запросов второго типа: \n" +
+            "FIFO $countOfAnswersFifo \n" +
+            "LRU $countOfAnswersLru \n" +
+            "OPT $countOfAnswersOpt \n"
+    answer.writeText(answerContent)
+}
+
+fun partOfAnswerAndCountOf2Type(algo: MutableList<Int>): Pair<Int, String> {
+    var answerContent = ""
+    var countOfAnswers2type = 0
+    for (i in algo) {
+        answerContent += i.toString() + "\n"
+        if (i != -1) countOfAnswers2type++
+    }
+    return Pair(countOfAnswers2type, answerContent)
+}
+
+fun getInput(): Pair<Int, MutableList<Int>> {
+    val memorySize = fileToList("inpout/conf.txt")[0].toInt()
+
+    val commandsString = fileToList("inpout/commands.txt")
+    val commands = mutableListOf<Int>()
+    for (i in commandsString) {
+        commands.add(i.toInt())
+    }
+    return Pair(memorySize, commands)
+}
+
+fun main() {
+    val (memorySize, commands) = getInput()
+    makeAnswer(memorySize, commands)
 }
